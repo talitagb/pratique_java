30fac4b1b45d5a1af82849f53ff21fb2faa241c9
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-08-23 20:06:36 -0300

    Primeiro commit criando uma classe para um exemplo que casting

---
+++ src/exercicios/Main.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		
+		//Exemplo de casting (que é a inversão explicita de valores)
+		resultado = (double) a/b;
+		
+		System.out.println(resultado);
+
+	}
+
+}
