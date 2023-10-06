163e93c8f7ac9e1d4649de450ef8260a3f110d37
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-10-06 10:59:54 -0300

    Exercício sobre entrada, processamento e saída de dados

---
+++ src/exercicios/ExercicioSalário.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioSalário {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int numFuncionario, horasTrabalhadas;
+		double  valorPorHora, salario;
+		
+		System.out.println("Digite o número do funcionário, horas trabalhadas e valor que recebe por hora: ");
+		numFuncionario = sc.nextInt();
+		horasTrabalhadas = sc.nextInt();
+		valorPorHora = sc.nextDouble();
+		
+		salario = horasTrabalhadas * valorPorHora;
+		
+		System.out.println("Número Funcionário = " + numFuncionario);
+		System.out.printf("Salário = R$ %.2f", salario);
+		
+		sc.close();
+	}
+
+}
