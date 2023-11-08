9bfcf78d3863877cfd32cb25cbc053cad0c9fa95
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-11-08 18:37:45 -0300

    Programa Conta, usando encapsulamento, contrutores e getters e setters

---
+++ exerciciospoo/application/ProgramaBanco.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Conta;
+
+public class ProgramaBanco {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Este programa simples que simula uma conta no banco, podendo depositar um depósito inicial ou não,
+		 * depositar uma quantia e sacar uma quantia.
+		 * 
+		 */
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Conta conta;
+		
+		System.out.println("Insira o número da conta: ");
+		int numeroConta = sc.nextInt();	
+		System.out.println("Insira o nome do titular: ");
+		sc.nextLine();
+		String titular = sc.nextLine();
+		
+		System.out.println("Há um depósito inicial (s/n)? ");
+		char resposta = sc.next().charAt(0);
+		
+		if(resposta == 's') {
+			System.out.println("Insira o deposito inicial: ");
+			double depositoInicial = sc.nextDouble();
+			conta = new Conta (numeroConta, titular, depositoInicial);
+		}
+		else {
+			conta = new Conta(numeroConta, titular);
+		}
+		
+		System.out.println(conta);
+		System.out.println();
+		System.out.println("Insira um valor para deposito: ");
+		double quantia = sc.nextDouble();
+		conta.deposito(quantia);
+		System.out.println("Conta Atualizada :" + conta);
+		System.out.println();
+		System.out.println("Insira um valor para saque: ");
+		quantia = sc.nextDouble();
+		conta.saque(quantia);
+		System.out.println("Conta Atualizada :" + conta);
+		
+		
+		sc.close();
+	}
+
+}
---
+++ exerciciospoo/entities/Conta.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class Conta {
+
+	private int numeroConta;
+	private String titular;
+	private double saldo;
+
+	public Conta(int numeroConta, String titular) {
+		this.numeroConta = numeroConta;
+		this.titular = titular;
+	}
+	
+	public Conta(int numeroConta, String titular, double depositoInicial) {
+		this.numeroConta = numeroConta;
+		this.titular = titular;
+		deposito(depositoInicial);
+	}
+	
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	
+	public void deposito(double quantia) {
+		saldo += quantia;
+		
+	}
+	
+	public void saque(double quantia) {
+		saldo -= quantia + 5.0;
+	}
+	
+	public String toString() {
+		return "Conta "
+				+ numeroConta
+				+ ", Titular: "
+				+ titular
+				+ ", Saldo: R$ "
+				+ String.format("%.2f", saldo); 
+	}
+}
