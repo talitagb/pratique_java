f55bf69fd505cda66cb20dba6aadc33449de569c
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-11-09 13:24:24 -0300

    Exercício Negativos sobre vetores

---
+++ exerciciospoo/application/ProgramaNegativos.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramaNegativos {
+
+	public static void main(String[] args) {
+		// Este programa lê um número inteiro n e depois n números inteiros e
+		// armazena-os em um vetor. Depois mostra na tela os números negativos lidos
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Quantos números você vai digitar? ");
+		int n = sc.nextInt();
+
+		int[] vetor = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Digite um número: ");
+			vetor[i] = sc.nextInt();
+		}
+
+		System.out.println("Numeros Negativos: ");
+
+		for (int i = 0; i < n; i++) {
+			if (vetor[i] < 0) {
+				System.out.println(vetor[i]);
+			}
+		}
+
+		sc.close();
+	}
+
+}
