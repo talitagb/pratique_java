04c45690d569ea6831d69d9a87bffd9e3f6d95b1
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-11-13 16:01:39 -0300

    Ajustes no programa alturas

--- exerciciospoo/application/ProgramaAlturas.java
+++ exerciciospoo/application/ProgramaAlturas.java
@@ -6,22 +6,23 @@ import java.util.Scanner;
 public class ProgramaAlturas {
 
 	public static void main(String[] args) {
+		/*
+		 * Este programa lê o nome, idade e altura de N pessoas. Depois, mostrar na tela a altura média das pessoas,
+		 * e mostrar também a porcentagem de pessoas com menos de 16 anos, bem como os nomes dessas
+		 * pessoas caso houver.
+		 */
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Quantas pessoas serão digitadas? ");
-		int n, nmenores;
-		double alturatotal, alturamedia, percentualMenores;
-
-		System.out.print("Quantas pessoas serao digitadas? ");
-		n = sc.nextInt();
+		System.out.println("Quantas pessoas serão digitadas?");
+		int n = sc.nextInt();
 
 		String[] nomes = new String[n];
 		int[] idades = new int[n];
 		double[] alturas = new double[n];
 
 		for (int i = 0; i < n; i++) {
-			System.out.printf("Dados da %da pessoa:\n", i + 1);
+			System.out.printf("Dados da pessoa %d %n", i + 1);
 			System.out.print("Nome: ");
 			nomes[i] = sc.next();
 			System.out.print("Idade: ");
@@ -30,26 +31,27 @@ public class ProgramaAlturas {
 			alturas[i] = sc.nextDouble();
 		}
 
-		nmenores = 0;
-		alturatotal = 0;
+		double soma = 0;
+		int contador = 0;
 		for (int i = 0; i < n; i++) {
 			if (idades[i] < 16) {
-				nmenores++;
+				contador++;
 			}
-			alturatotal = alturatotal + alturas[i];
+			soma += alturas[i];
 		}
+		double media = soma / n;
+		double porcentagem = ((double) contador / n) * 100;
 
-		alturamedia = alturatotal / n;
-		percentualMenores = ((double) nmenores / n) * 100.0;
-
-		System.out.printf("\nAltura media = %.2f\n", alturamedia);
-		System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
+		System.out.printf("Altura média: %.2f%n", media);
+		System.out.println("Pessoas com menos de 16 anos: " + porcentagem + "%");
 
 		for (int i = 0; i < n; i++) {
 			if (idades[i] < 16) {
-				System.out.printf("%s\n", nomes[i]);
+				System.out.println(nomes[i]);
+				;
 			}
 		}
+
 		sc.close();
 	}
 
