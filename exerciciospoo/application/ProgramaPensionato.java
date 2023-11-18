3c8d29d706e399026913d21188d9cb088cc7d111
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-11-17 22:22:27 -0300

    Ajustes

--- exerciciospoo/application/ProgramaPensionato.java
+++ exerciciospoo/application/ProgramaPensionato.java
@@ -10,7 +10,7 @@ public class ProgramaPensionato {
 		/*
 		 * A dona de um pensionato possui dez quartos para alugar para estudantes, sendo
 		 * esses quartos identificados pelos números 0 a 9.
-		 *  Este programa inicia com todos os dez quartos vazios, e depois leia uma quantidade N representando o
+		 * Este programa inicia com todos os dez quartos vazios, e depois leia uma quantidade N representando o
 		 * número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em
 		 * seguida, registra o aluguel dos N estudantes. Para cada registro de aluguel,
 		 * informar o nome e email do estudante, bem como qual dos quartos ele escolheu
