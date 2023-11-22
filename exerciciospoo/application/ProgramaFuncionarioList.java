a4b86be765178c4350a86e52fbfcd5fb4fa4e893
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2023-11-22 16:06:48 -0300

    Exercício sobre listas

---
+++ exerciciospoo/application/ProgramaFuncionarioList.java
@@ -0,0 +1,70 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.FuncionarioList;
+
+public class ProgramaFuncionarioList {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		List<FuncionarioList> list = new ArrayList<>();
+
+		// PARTE 1 - Lendo dados:
+
+		System.out.print("Quantos funcionários você quer registrar? ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Funcionário #" + i + ": ");
+
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			while (hasId(list, id)) {
+				System.out.print("Esse ID já possui, tente novamente: ");
+				id = sc.nextInt();
+			}
+
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			System.out.print("Salário: ");
+			double salario = sc.nextDouble();
+			list.add(new FuncionarioList(id, nome, salario));
+		}
+
+		// PARTE 2 - Atualizando o salário de determinado empregado:
+
+		System.out.println();
+		System.out.print("Indique o ID do funcionário que terá aumento salarial:");
+		int id = sc.nextInt();
+		FuncionarioList func = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if (func == null) {
+			System.out.println("Este id não existe!");
+		} else {
+			System.out.print("Indique a porcentagem");
+			double percentage = sc.nextDouble();
+			func.incremento(percentage);
+		}
+
+		// PARTE 3 - Listando funcionários:
+
+		System.out.println();
+		System.out.println("List of employees:");
+		for (FuncionarioList obj : list) {
+			System.out.println(obj);
+		}
+
+		sc.close();
+	}
+
+	public static boolean hasId(List<FuncionarioList> list, int id) {
+		FuncionarioList func = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return func != null;
+	}
+
+}
---
+++ exerciciospoo/entities/FuncionarioList.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class FuncionarioList {
+
+	private Integer id;
+	private String nome;
+	private Double salario;
+
+	public FuncionarioList() {
+	}
+
+	public FuncionarioList(Integer id, String nome, Double salario) {
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public void incremento(double porcentagem) {
+		salario += salario * porcentagem / 100.0;
+	}
+
+	public String toString() {
+		return id + ", " + nome + ", " + String.format("%.2f", salario);
+	}
+}
